cc4718a7a639ae08c8a2c9e143ab92aba2028617
Lee-Michael D'Souza <63876944+rimichaeru@users.noreply.github.com>
2021-07-13 09:48:15 +0100

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,47 @@
+// function to check for prime
+const isPrime = (numberLimit) => {
+  for (let i = 2; i < numberLimit; i++) {
+    if (numberLimit % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Get FIZZBUZZ string results
+let stringResult = "";
+for (let i = 1; i < 501; i++) {
+  if (isPrime(i)) {
+    stringResult += "FiZZBUZZ++\n";
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    stringResult += "FIZZBUZZ\n";
+  } else if (i % 3 === 0) {
+    stringResult += "FIZZ\n";
+  } else if (i % 5 === 0) {
+    stringResult += "BUZZ\n";
+  }
+}
+
+// write/append fizzbuzz.log with the results
+const fs = require("fs");
+try {
+
+  // append to file if it already exists
+  if (fs.existsSync("./tech-test/fizzbuzz.log")) {
+    fs.appendFile("./tech-test/fizzbuzz.log", stringResult, (error) => {
+      if (error) {
+        return console.error(error);
+      }
+    });
+
+  // write to new file if it doesn't exist
+  } else {
+    fs.writeFile("./tech-test/fizzbuzz.log", stringResult, (error) => {
+      if (error) {
+        return console.error(error);
+      }
+    });
+  }
+} catch(error) {
+  console.error(error)
+}
